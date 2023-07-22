e6ec150eef8fb490375a7716910ff93311eeb0cf
imransarkerweb <imransarker.web@gmail.com>
2023-07-23 01:08:41 +0600

    header and footer added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../pages/shared/NavBar/NavBar";
+import Footer from "../pages/shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <div className="w-[90%] mx-auto">
+        <NavBar></NavBar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;

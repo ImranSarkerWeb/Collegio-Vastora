b6a259ebaea83b52f9af7cbc7ffb58c5d17ef425
imransarkerweb <imransarker.web@gmail.com>
2023-07-24 20:45:20 +0600

    updated

--- src/pages/shared/Loader/Loader.jsx
+++ src/pages/shared/Loader/Loader.jsx
@@ -1,5 +1,16 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="text-center">
+      <Player
+        className="w-60 "
+        autoplay
+        loop
+        src="https://lottie.host/223d1ee9-3780-46e2-8924-291255492a7c/Ahd3Vsemng.json"
+      ></Player>
+    </div>
+  );
 };
 
 export default Loader;
